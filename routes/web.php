2dc1270d42f004a8170b89393c8b02d62be927ab
ankit5 <vermankit5@gmail.com>
2024-07-09 12:20:33 +0530

    asd

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,22 @@ use Illuminate\Support\Facades\Route;
 
 // Movies
 Route::middleware('page-cache')->get('/', 'MoviesController@index')->name('movies.index');
-Route::get('/movies', 'MoviesController@movies')->name('movies.movies');
-Route::get('/search/{search}', 'MoviesController@search')->name('movies.search');
-Route::get('/search', 'MoviesController@search2')->name('movies.search2');
-Route::get('/tv-series', 'MoviesController@series')->name('movies.series');
-Route::get('/movies/{id}', 'MoviesController@show')->name('movies.show');
+Route::middleware('page-cache')->get('/movies', 'MoviesController@movies')->name('movies.movies');
+Route::middleware('page-cache')->get('/search/{search}', 'MoviesController@search')->name('movies.search');
+Route::middleware('page-cache')->get('/search', 'MoviesController@search2')->name('movies.search2');
+Route::middleware('page-cache')->get('/tv-series', 'MoviesController@series')->name('movies.series');
+Route::middleware('page-cache')->get('/movies/{id}', 'MoviesController@show')->name('movies.show');
 
-Route::get('/years/{year}', 'MoviesController@year')->name('movies.year');
-Route::get('/country/{country}', 'MoviesController@country')->name('movies.country');
-Route::get('/actor/{actor}', 'MoviesController@actor')->name('movies.actor');
-Route::get('/director/{director}', 'MoviesController@director')->name('movies.director');
-Route::get('/networks/{network}', 'MoviesController@network')->name('movies.network');
-Route::get('/genre/{tag}', 'MoviesController@tag')->name('movies.tag');
-Route::get('/letter/{latter}', 'MoviesController@letter')->name('movies.letter');
-Route::get('/networks', 'MoviesController@networks')->name('movies.networks');
-Route::get('/collection', 'MoviesController@collection')->name('movies.collection');
-Route::get('/collection/{collection}', 'MoviesController@collection_search')->name('movies.collection_search');
+Route::middleware('page-cache')->get('/years/{year}', 'MoviesController@year')->name('movies.year');
+Route::middleware('page-cache')->get('/country/{country}', 'MoviesController@country')->name('movies.country');
+Route::middleware('page-cache')->get('/actor/{actor}', 'MoviesController@actor')->name('movies.actor');
+Route::middleware('page-cache')->get('/director/{director}', 'MoviesController@director')->name('movies.director');
+Route::middleware('page-cache')->get('/networks/{network}', 'MoviesController@network')->name('movies.network');
+Route::middleware('page-cache')->get('/genre/{tag}', 'MoviesController@tag')->name('movies.tag');
+Route::middleware('page-cache')->get('/letter/{latter}', 'MoviesController@letter')->name('movies.letter');
+Route::middleware('page-cache')->get('/networks', 'MoviesController@networks')->name('movies.networks');
+Route::middleware('page-cache')->get('/collection', 'MoviesController@collection')->name('movies.collection');
+Route::middleware('page-cache')->get('/collection/{collection}', 'MoviesController@collection_search')->name('movies.collection_search');
 
 
 
